21956d592bb678d30f263cd0b90292f57a53eb0e
CICEK Orhan <orhancicek1985@gmail.com>
2026-03-19 17:03:21 +0100

    ajout Login et logOut manuelle

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function create() {
+        return view('auth.login');
+    }
+
+    public function store() {
+        // validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        // attempt to login the user
+        if(! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials do not match.'
+            ]);
+        }
+
+        // regenerate the sesison token
+        request()->session()->regenerate();
+
+        // redirect
+        return redirect('/jobs');
+    }
+
+    public function destroy() {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUsercontroller;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUsercontroller::class, 'create']);
+Route::post('/register', [RegisterUsercontroller::class, 'store']);
+
+route::get('/login', [SessionController::class, 'create']);
+route::post('/login', [SessionController::class, 'store']);
+route::post('/logout', [SessionController::class, 'destroy']);
+
+
